7e604f909a11f5c0a774d7e914035d4a13c550aa
Ayushi-aashish <ayushiaashish865@gmail.com>
2026-06-16 17:48:41 +0530

    added the task features

--- src/main/java/com/example/demo/entity/task.java
+++ src/main/java/com/example/demo/entity/task.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "Task")
 public class task {
@@ -9,4 +12,17 @@ public class task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private priority Priority;
+    @Enumerated(EnumType.STRING)
+    private status Status;
+    private LocalDate dueDate;
+    private LocalDateTime createdAt;
+    private LocalDate updatedAt;
+
+
+
+
 }
